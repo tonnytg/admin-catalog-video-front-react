226ed12de686f4beb5ae4ccddb9f8b5343930992
Antonio Thomacelli <tonnytg@gmail.com>
2022-03-14 21:26:36 -0300

    add blue navbar

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import {AppBar, Button, Toolbar, Typography} from "@mui/material";
+import logo from "./public/logo.png"
+export const Navbar = () => {
+    return (
+        <AppBar>
+            <Toolbar>
+                <Typography>
+                    <img src="" alt="" />
+                </Typography>
+                <Button />
+            </Toolbar>
+        </AppBar>
+    );
+};
