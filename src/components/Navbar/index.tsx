90e780190c5d71fc74fc15962867c0f4a2a3462f
Antonio Thomacelli <tonnytg@gmail.com>
2022-03-15 22:11:49 -0300

    fix package core and responsive logo

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,13 +1,23 @@
 import * as React from 'react';
-import {AppBar, Button, Toolbar, Typography} from "@mui/material";
-import { makeStyles } from '@mui/styles';
+import {AppBar, Button, Toolbar, Typography, Theme, makeStyles } from "@material-ui/core";
 import logo from "../../static/img/logo.png"
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme: Theme) => ({
     toolbar:{
         backgroundColor: '#000000'
+    },
+    title: {
+        flexGrow: 1,
+        textAlign: 'center'
+    },
+    logo: {
+        width: 100,
+        // gera um media query
+        [theme.breakpoints.up('sm')]: {
+            width: 170
+        }
     }
-})
+}));
 
 export const Navbar = () => {
     const classes = useStyles();
@@ -15,8 +25,8 @@ export const Navbar = () => {
     return (
         <AppBar>
             <Toolbar className={classes.toolbar}>
-                <Typography>
-                    <img src={logo} alt="Codeflix" />
+                <Typography className={classes.title}>
+                    <img src={logo} alt="Codeflix" className={classes.logo}/>
                 </Typography>
                 <Button color="inherit" >Login</Button>
 
