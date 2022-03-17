82378a0e35984bd5850e15c9d6736563bed72ea7
Antonio Thomacelli <tonnytg@gmail.com>
2022-03-16 22:56:56 -0300

    add menu with itens

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import {AppBar, Button, Toolbar, Typography, Theme, makeStyles } from "@material-ui/core";
+import {AppBar, Button, Toolbar, Typography, Theme, makeStyles, IconButton, Menu, MenuItem} from "@material-ui/core";
+import MenuIcon from "@mui/icons-material/Menu"
 import logo from "../../static/img/logo.png"
 
 const useStyles = makeStyles((theme: Theme) => ({
@@ -21,10 +22,40 @@ const useStyles = makeStyles((theme: Theme) => ({
 
 export const Navbar = () => {
     const classes = useStyles();
-    console.log(classes)
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleOpen = (event: any) => setAnchorEl(event.currentTarget);
+    const handleClose = () => setAnchorEl(null);
+
     return (
         <AppBar>
             <Toolbar className={classes.toolbar}>
+                <IconButton
+                    edge='start'
+                    color='inherit'
+                    aria-label='open drawer'
+                    aria-controls='menu-appbar'
+                    aria-haspopup="true"
+                    onClick={handleOpen}
+                >
+                    < MenuIcon />
+                </IconButton>
+
+                <Menu
+                    id="menu-appbar"
+                    open={open}
+                    anchorEl={anchorEl}
+                    onClose={handleClose}
+                    anchorOrigin={{vertical: 'bottom', horizontal: 'center'}}
+                    transformOrigin={{vertical: 'top', horizontal:'center'}}
+                    getContentAnchorEl={null}
+                >
+                    <MenuItem>
+                        Categorias
+                    </MenuItem>
+                </Menu>
+
                 <Typography className={classes.title}>
                     <img src={logo} alt="Codeflix" className={classes.logo}/>
                 </Typography>
